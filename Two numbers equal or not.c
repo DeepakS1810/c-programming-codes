3d3e0bcf3582d1d5c5028ee6dcc06243a4d62061
DeepakS1810 <s.deepak1810@gmail.com>
2025-08-05 10:54:08 +0530

    Create Two numbers equal or not.c

---
+++ Two numbers equal or not.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+int a,b;
+scanf("%d %d",&a,&b);
+if (a==b)
+printf("The given numbers are equal..\n");
+else
+printf("The given numbers are not equal..\n"); return 0;
+}
